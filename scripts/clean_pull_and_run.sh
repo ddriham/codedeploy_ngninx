f95b39fb9c346ca357c6bbf54730e2288f6b97a3
ddriham <121961609+ddriham@users.noreply.github.com>
2024-01-01 20:22:13 +0200

    Update clean_pull_and_run.sh
    create another step on appspec.yaml

--- scripts/clean_pull_and_run.sh
+++ scripts/clean_pull_and_run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-sudo docker stop image-nginx &> /dev/null
-sudo docker rm image-nginx &> /dev/null
-sudo docker rmi nginx:latest &> /dev/null
-
-
 sudo docker pull nginx:latest
 sudo docker run -itd --name image-nginx -p 80:80 nginx:latest
